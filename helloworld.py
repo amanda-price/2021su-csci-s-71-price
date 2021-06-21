292c799d54d3c1187fcffe68a2ae81e7975cdc8c
Amanda Price <a.price.138@gmail.com>
2021-06-21 17:29:53 -0500

    add helloworld screenshot for assignment #1

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def print_hi(name):
+    print(f'Hello, {name}!')  # Press ⌘F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    name = sys.argv[1]
+    print_hi(name)
+
